54e14d67623e6f705efcde5f522a4f06c0dc08bd
Naell <codesphereum@gmail.com>
2022-07-14 09:57:16 -0300

    add files

---
+++ javascript/exercises/jschallenger/javascript-fundamentals/javascript-basics/05-remove-first-n-characters-of-string.js
@@ -0,0 +1,10 @@
+/*
+Remove first n characters of string
+Write a function that takes a string (a) as argument
+Remove the first 3 characters of a
+Return the result
+*/
+
+function myFunction(a) {
+    return a.slice(3);
+}
---
+++ javascript/exercises/jschallenger/javascript-fundamentals/javascript-basics/06-get-last-n-characters-of-string.js
@@ -0,0 +1,10 @@
+/*
+Get last n characters of string
+Write a function that takes a string as argument
+Extract the last 3 characters from the string
+Return the result
+*/
+
+function myFunction(str) {
+    return str.slice(-3);
+}
